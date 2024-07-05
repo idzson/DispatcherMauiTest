104e9d7cb617274f2f809e5e830428104228779d
aid <aid@spyro-soft.com>
2024-07-05 11:21:51 +0200

    Create project

---
+++ MauiDispatcherTest/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiDispatcherTest;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        Dispatcher.Dispatch(() =>
+        {
+            MainPage = new AppShell();
+        });
+        // MainThread.BeginInvokeOnMainThread(() =>
+        // {
+        //     MainPage = new AppShell();
+        //});
+    }
+}
---
+++ MauiDispatcherTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDispatcherTest;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
